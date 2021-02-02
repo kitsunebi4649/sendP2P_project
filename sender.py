a7dcd868afb608db7797037beb090cb045a597ae
kitsunebi4649 <tsubamon@au.com>
2021-02-02 18:21:48 +0900

    画像の送信に成功

--- sender.py
+++ sender.py
@@ -1,10 +1,15 @@
+import os
 import requests
 import utils
 
-FILE_NAME = 'SAMPLE.png'
 RECIPIENT_IP = '192.168.0.6:5004'
 
 
 if __name__ == '__main__':
-    data = utils.read_and_encode(FILE_NAME)
-    requests.put(f'http://{RECIPIENT_IP}/test', json=data)
+    filename_list = os.listdir('send_file')
+    print(filename_list)
+    if not filename_list == []:
+        data = utils.read_and_encode(filename_list[0])
+        requests.put(f'http://{RECIPIENT_IP}/test', json=data)
+    else:
+        print('no file!!')
--- utils.py
+++ utils.py
@@ -2,12 +2,12 @@ import base64
 
 
 def read_and_encode(filename):
-    with open(filename, 'br') as f1:
+    with open(f'send_file/{filename}', 'br') as f1:
         b64_img = base64.b64encode(f1.read())
     send_data = b64_img.decode()
     return send_data
 
 
 def decode_and_write(new_filename, data):
-    with open(new_filename, 'bw') as f4:
+    with open(f'receive_file/{new_filename}', 'bw') as f4:
         f4.write(base64.b64decode(data))
