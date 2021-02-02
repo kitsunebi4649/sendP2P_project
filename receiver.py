0f8d15ce17c7a2690205f7b5564846c7d91311f2
kitsunebi4649 <tsubamon@au.com>
2021-02-02 20:21:17 +0900

    複数ファイルに対応

--- receiver.py
+++ receiver.py
@@ -11,8 +11,10 @@ if __name__ == '__main__':
     response = requests.get(f'http://127.0.0.1:{MY_PORT}/test')  # blockchain.pyの222行目参照
     response_json = response.json()
     if response.status_code == 200:
-        data = response_json['data']
-        new_filename = response_json['filename']
-        print(data)
-        for k, v in zip(new_filename, data):
+        data = response_json
+        for k, v in data.items():
             utils.decode_and_write(new_filename=k, data=v)
+        # new_filename = response_json['filename']
+        # print(data)
+        # for k, v in zip(new_filename, data):
+        #     utils.decode_and_write(new_filename=k, data=v)
--- sender.py
+++ sender.py
@@ -7,16 +7,11 @@ RECIPIENT_IP = '192.168.0.6:5004'
 
 if __name__ == '__main__':
     filename_list = os.listdir('send_file')
-    filename_list.remove('.DS_Store')  # .DS_Storeを削除
-    print(filename_list)
-    # data_list = []
-    # for filename in filename_list:
-    #     data_list.append(utils.read_and_encode(filename))
-    data_list = [utils.read_and_encode(filename) for filename in filename_list]
     if not filename_list == []:
-        data = {'data': data_list,
-                'filename': filename_list
-                }  # 個々でリストにするか検討する
+        filename_list.remove('.DS_Store')  # .DS_Storeを削除
+        data_list = [utils.read_and_encode(filename) for filename in filename_list]
+        print(filename_list)
+        data = {k: v for k, v in zip(filename_list, data_list)}
         requests.put(f'http://{RECIPIENT_IP}/test', json=data)
     else:
         print('no file!!')
