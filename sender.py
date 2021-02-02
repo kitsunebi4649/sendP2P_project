1bcd03e2bab13c5a6a4f53f6f231c4ebf38ab934
kitsunebi4649 <tsubamon@au.com>
2021-02-02 19:12:32 +0900

    名前を共有

--- sender.py
+++ sender.py
@@ -7,10 +7,11 @@ RECIPIENT_IP = '192.168.0.6:5004'
 
 if __name__ == '__main__':
     filename_list = os.listdir('send_file')
+    filename_list.remove('.DS_Store')  # .DS_Storeを削除
     print(filename_list)
     if not filename_list == []:
-        data = {'data':utils.read_and_encode(filename_list[0]),
-                'filename':filename_list[0]
+        data = {'data': utils.read_and_encode(filename_list[0]),
+                'filename': filename_list[0]
                 }
         requests.put(f'http://{RECIPIENT_IP}/test', json=data)
     else:
